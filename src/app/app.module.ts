9810f1dbf1dfa445013cc5c9a442a2eccede057c
Sreeram Sama <12038441+sreeramsama@users.noreply.github.com>
2023-06-06 14:03:30 -0700

    chore: add ng14app with Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AmplifyAuthenticatorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Amplify } from 'aws-amplify';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,9 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
